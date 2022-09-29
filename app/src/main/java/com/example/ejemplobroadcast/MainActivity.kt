65923d6e5ce16f4390d57873a0e1394d87044cb4
Leandro <leo250703@gmail.com>
2022-09-29 08:45:34 -0400

    Commit 29/09/2022 08:45 (Leandro Morales) -> Broadcast Receiver

--- app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.ejemplobroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejemplobroadcast.databinding.ActivityMainBinding
@@ -9,6 +12,23 @@ class MainActivity : AppCompatActivity() {
     //Variables
 
     private lateinit var binding: ActivityMainBinding
+
+    //variable que sriva para configurar un broadcastReceiver
+    //en este caso particular para comunicarse con el sistema
+    //y saber si esta activo o no el modo avion
+
+    private val getAirplaneMode = object: BroadcastReceiver(){
+
+        //Al momento de configurar un BroadcastReceiver
+        //es fundamental que ustedes sobreescriban el metodo
+        //llamado onReceive -> posibilidad de recibir informacion
+        //del evento del sistema y ustedes definen la logica
+        // que desean aplicar a partir de ese evento y esa informacion
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            TODO("Not yet implemented")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
